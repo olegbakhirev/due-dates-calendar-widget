7be17e70407fdc0ef03c0ecb83e12c898230e230
Oleg Bakhirev <oleg.bakhirev@jetbrains.com>
2020-11-16 22:17:39 +0300

    Update localisation

--- src/app/app.js
+++ src/app/app.js
@@ -1,19 +1,18 @@
 import 'babel-polyfill';
 import DashboardAddons from 'hub-dashboard-addons';
-import {setLocale} from 'hub-dashboard-addons/dist/localization';
 import React from 'react';
 import {render} from 'react-dom';
 
 import DueDatesCalendarWidget from './due_dates_calendar_widget';
 import ConfigWrapper from './config-wrapper';
-import TRANSLATIONS from './translations';
+import {initTranslations} from './translations';
 
 const CONFIG_FIELDS = [
   'search', 'context', 'title', 'refreshPeriod', 'youTrack', 'date', 'view', 'scheduleField', 'colorField', 'isDateAndTime'
 ];
 
 DashboardAddons.registerWidget(async (dashboardApi, registerWidgetApi) => {
-  setLocale(DashboardAddons.locale, TRANSLATIONS);
+  initTranslations(DashboardAddons.locale);
   const configWrapper = new ConfigWrapper(dashboardApi, CONFIG_FIELDS);
 
   render(
